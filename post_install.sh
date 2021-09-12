755f52d974e1e482114c9d9c5af767c15a39e112
Michael-T-L <michael.todd001@gmail.com>
2021-09-12 08:28:48 -0600

    Fixing path, adding module to install

--- post_install.sh
+++ post_install.sh
@@ -17,7 +17,16 @@ if [ -e "/etc/iocage-env" ] ; then
 	echo "Using NAT Address: $IOCAGE_PLUGIN_IP"
 fi
 
-# Configure ampache
+# Configure apache and php
+rm -rf /usr/local/www/apache24/data
+ln -s /usr/local/www/ampache/public /usr/local/www/apache24/data
+
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+
+sed -i '' 's/.*LoadModule rewrite_module libexec/apache24/mod_rewrite.so/LoadModule rewrite_module libexec/apache24/mod_rewrite.so/' /usr/local/etc/apache24/httpd.conf
+sed -i '' 's/.*LoadModule php7_module\s*libexec/apache24/libphp7.so/LoadModule php7_module libexec/apache24/libphp7.so/' /usr/local/etc/apache24/httpd.conf
+sed -i '' 's/DirectoryIndex index.html/DirectoryIndex index.html index.php/' /usr/local/etc/apache24/httpd.conf
+echo '<FilesMatch "\.php$">\n    SetHandler application/x-httpd-php\n</FilesMatch>\n<FilesMatch "\.phps$">\n    SetHandler application/x-httpd-php-source\n</FilesMatch>' >> /usr/local/etc/apache24/httpd.conf
 
 # Configure database
 # Note - Ampache's web-based post-intall will set this up
